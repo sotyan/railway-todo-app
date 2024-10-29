e54433b85820e55711c3a42616b0a2e86b3d95ff
sotyan <happy.tanbarin2@gmail.com>
2024-10-29 09:01:04 +0900

    コードを整えた(npm run fix)

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
-import { createRoot } from "react-dom/client"
+import { store } from "./store";
+import { createRoot } from "react-dom/client";
 
 // ReactDOM.render(
-  createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
   </Provider>,
-  );
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
